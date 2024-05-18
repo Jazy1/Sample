236064f34061b33ca8a402d29bbb125c7a7b60f3
abdullahabid69 <abdullahabidwahcantt@gmail.com>
2024-05-18 14:23:35 +0500

    nothing

--- team/routes/web.php
+++ team/routes/web.php
@@ -6,6 +6,10 @@ Route::get('/addquiz', function () {
     return view('teacher.addquiz');
 });
 
+Route::get('/takequiz', function () {
+    return view('student.takequiz');
+});
+
 
 
 Route::get('/teacher', function () {
